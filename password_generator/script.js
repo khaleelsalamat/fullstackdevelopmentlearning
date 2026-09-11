4af6ccca9606df486556fb54ffd494f222e134df
khaleelalsalamat <khaleelsalamat24@gmail.com>
2026-09-11 18:27:23 +0300

    the password generator

---
+++ password_generator/script.js
@@ -0,0 +1,15 @@
+const passwordLength = 12;
+const passwordnm = [];
+const password=document.getElementById('password');
+const characters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+'0','1','2','3','4','5','6','7','8','9',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+'!','#','$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', 
+'~'];
+function generatePassword() {
+    for (let i = 0; i < passwordLength; i++) {
+     passwordnm [i]= characters[Math.floor(Math.random() * characters.length)];} 
+     password.innerHTML=passwordnm.join('')
+     
+       
+}
