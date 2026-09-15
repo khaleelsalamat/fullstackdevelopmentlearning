fddff38e5dc820ac0b5f3343488e492b7ec9b111
khaleelalsalamat <khaleelsalamat24@gmail.com>
2026-09-15 16:26:17 +0300

    try this

---
+++ unit_converter/script.js
@@ -0,0 +1,14 @@
+let data=[{
+    player: "jane",
+    team: "India",
+    score: 85
+
+},
+{
+    player: "Rashid Khan",
+    team: "Afghanistan",
+    role: "Bowler"
+}]
+login.addEventListener('click',function(){
+    console.log(`jane score is ${jane.score}`);
+    ;})
